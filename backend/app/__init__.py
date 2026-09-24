bf56bce8f009d485b478c53dff4f5f6fc69a8667
github/rati-yadav <ratiyadav968@gmail.com>
2026-09-25 01:20:48 +0530

    Fixed CORS Errors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,8 +5,13 @@ from .config import Config
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
-
+    CORS(app,
+    origins=[
+        "http://localhost:3000",
+        "https://taskmanagerappraj.vercel.app"
+    ],
+    supports_credentials=True
+)
     app.config.from_object(Config)
 
     # Allow requests from the Next.js frontend
